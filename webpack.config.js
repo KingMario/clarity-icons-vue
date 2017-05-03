33de6bdd5a6c58f283be969c64269ec6848fb4d9
cgeng <cgeng@vmware.com>
2017-05-03 16:18:04 +0800

    Correct typo

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].min.js',
-    library: 'webpackNumbers',
+    library: '[name]',
     libraryTarget: 'umd'
   },
   resolve: {
